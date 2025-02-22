b2875522face2b71b21142b18836acf9d1884ffd
Ashis Kumar Panda <ashiskumarpanda727@gmail.com>
2025-02-22 21:19:55 +0530

    Add files via upload

---
+++ 05 Docker/a01-springboot_with_docker/src/main/java/com/example/a01_springboot_with_docker/A01SpringbootWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.a01_springboot_with_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A01SpringbootWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A01SpringbootWithDockerApplication.class, args);
+	}
+
+}
---
+++ 05 Docker/a01-springboot_with_docker/src/test/java/com/example/a01_springboot_with_docker/A01SpringbootWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a01_springboot_with_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A01SpringbootWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
